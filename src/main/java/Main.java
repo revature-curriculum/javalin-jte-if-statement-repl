34bc579f06228a0bc62758c563a0a0ab9ce010fd
Ashoka Shringla <ashokashringla@gmail.com>
2022-02-01 10:12:01 -0500

    added files

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import io.javalin.Javalin;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(4100);
+        app.get("/", ctx -> {
+            ctx.result("Hello! Look at the README.md for instructions.");
+        });
+
+        app.get("/whatAnimal", ctx -> {
+            ctx.render("animal.jte", Collections.singletonMap("name", "dog"));
+        });
+        
+    }
+}
